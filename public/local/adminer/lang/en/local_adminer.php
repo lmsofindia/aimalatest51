dfc6745a454ab89e1a576d0b589e512e46c33520
Mihir <lmsofindia@gmail.com>
2026-08-10 19:38:40 +0530

    Moodle adminer

---
+++ public/local/adminer/lang/en/local_adminer.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings
+ *
+ * @package    local_adminer
+ * @author Andreas Grabs <moodle@grabs-edv.de>
+ * @copyright  Andreas Grabs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+$string['additionalrelations'] = 'Additional relations';
+$string['additionalrelations_help'] = 'Additional relations to define that are not calculated from database files (install.xml). Put each relation on a new line. For example, see default value.';
+$string['adminer:useadminer'] = 'Use Adminer';
+$string['adminer_is_disabled_by_admin'] = 'Adminer is disabled by your administrator.';
+$string['adminer_secret'] = 'Adminer secret';
+$string['adminer_secret_note'] = 'Your administrator has defined an additional secret to use adminer. If you don\'t know it, please ask your administrator!';
+$string['blockinginfo'] = 'If you want to fully disable Adminer you can define "{$a}" as Adminer secret.';
+$string['cachedef_relations'] = 'Collected relations between tables';
+$string['config_startwithdb'] = 'Start adminer with current database';
+$string['example'] = 'Example';
+$string['example_key'] = 'your-secret-key';
+$string['pagenotusedinmoodle'] = 'This page can not be used in Moodle';
+$string['pluginname'] = 'Moodle Adminer';
+$string['privacy:metadata'] = 'The Adminer plugin does not store any personal data.';
+$string['securitynote'] = 'Security note';
+$string['securitynote_text'] = 'Due to the high security risk of using Adminer, an additional key can be defined in <strong>config.php</strong>.';
+$string['showquicklink'] = 'Show quick link';
+$string['showquicklink_help'] = 'Show a small icon on the navigation bar to open Adminer in a new tab.';
+$string['showrelationlinks'] = 'Show table relations';
+$string['showrelationlinks_help'] = 'Show table relations in separate column.';
+$string['showrelationlist'] = 'Show calculated relations from XMLDB ...';
+$string['source_col'] = 'Source column';
+$string['target_col'] = 'Target column';
+$string['wrong_adminer_secret'] = 'Wrong Adminer secret!';
